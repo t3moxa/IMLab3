7f161309c3ec29cf29c57bf91e9a17cb3215119a
t3moxa <vyachistyy@yandex.ru>
2024-03-07 16:39:29 +0700

    Upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            StartStopButton = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            RuleNumeric = new NumericUpDown();
+            RuleLabel = new Label();
+            TestLabel = new Label();
+            label1 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            ClearButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)RuleNumeric).BeginInit();
+            SuspendLayout();
+            // 
+            // StartStopButton
+            // 
+            StartStopButton.Location = new Point(256, 24);
+            StartStopButton.Name = "StartStopButton";
+            StartStopButton.Size = new Size(108, 23);
+            StartStopButton.TabIndex = 0;
+            StartStopButton.Text = "Старт/Стоп";
+            StartStopButton.UseVisualStyleBackColor = true;
+            StartStopButton.Click += StartStopButton_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 500;
+            timer1.Tick += timer1_Tick;
+            // 
+            // RuleNumeric
+            // 
+            RuleNumeric.Location = new Point(109, 12);
+            RuleNumeric.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
+            RuleNumeric.Name = "RuleNumeric";
+            RuleNumeric.Size = new Size(120, 23);
+            RuleNumeric.TabIndex = 2;
+            // 
+            // RuleLabel
+            // 
+            RuleLabel.AutoSize = true;
+            RuleLabel.Location = new Point(44, 14);
+            RuleLabel.Name = "RuleLabel";
+            RuleLabel.Size = new Size(59, 15);
+            RuleLabel.TabIndex = 3;
+            RuleLabel.Text = "Правило:";
+            // 
+            // TestLabel
+            // 
+            TestLabel.AutoSize = true;
+            TestLabel.Location = new Point(186, 44);
+            TestLabel.Name = "TestLabel";
+            TestLabel.Size = new Size(12, 15);
+            TestLabel.TabIndex = 4;
+            TestLabel.Text = "-";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(166, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Правило в двоичной форме:";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.BackColor = Color.White;
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            tableLayoutPanel1.ColumnCount = 16;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
+            tableLayoutPanel1.Location = new Point(14, 68);
+            tableLayoutPanel1.Margin = new Padding(0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 16;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 6.25F));
+            tableLayoutPanel1.Size = new Size(625, 625);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // ClearButton
+            // 
+            ClearButton.Location = new Point(370, 24);
+            ClearButton.Name = "ClearButton";
+            ClearButton.Size = new Size(75, 23);
+            ClearButton.TabIndex = 6;
+            ClearButton.Text = "Очистить";
+            ClearButton.UseVisualStyleBackColor = true;
+            ClearButton.Click += ClearButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(654, 700);
+            Controls.Add(ClearButton);
+            Controls.Add(label1);
+            Controls.Add(TestLabel);
+            Controls.Add(RuleLabel);
+            Controls.Add(RuleNumeric);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(StartStopButton);
+            Name = "Form1";
+            Text = "Lab3";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)RuleNumeric).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button StartStopButton;
+        private System.Windows.Forms.Timer timer1;
+        private NumericUpDown RuleNumeric;
+        private Label RuleLabel;
+        private Label TestLabel;
+        private Label label1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button ClearButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,148 @@
+using System.Windows.Forms;
+
 namespace IMLab3
 {
     public partial class Form1 : Form
     {
+        int i;
+        string ruleString;
+        private Dictionary<(bool, bool, bool), bool> ruleDictionary = new Dictionary<(bool, bool, bool), bool>();
         public Form1()
         {
             InitializeComponent();
+        }
+        private bool GetRule(int i)
+        {
+            if (Convert.ToBoolean(ruleString[i].Equals('0')))
+                return false;
+            else
+                return true;
+        }
+        private void SetRules()
+        {
+            ruleString = Convert.ToString(Convert.ToInt32(RuleNumeric.Value), 2);
+            while (ruleString.Length < 8)
+                ruleString = "0" + ruleString;
+            TestLabel.Text = ruleString;
+            ruleDictionary.Clear();
+            ruleDictionary.Add((false, false, false), GetRule(7));
+            ruleDictionary.Add((false, false, true), GetRule(6));
+            ruleDictionary.Add((false, true, false), GetRule(5));
+            ruleDictionary.Add((false, true, true), GetRule(4));
+            ruleDictionary.Add((true, false, false), GetRule(3));
+            ruleDictionary.Add((true, false, true), GetRule(2));
+            ruleDictionary.Add((true, true, false), GetRule(1));
+            ruleDictionary.Add((true, true, true), GetRule(0));
+        }
+        private bool GetCellCondition(int x, int y)
+        {
+            if (x == -1)
+                x = 15;
+            if (x == 16)
+                x = 0;
+            Label l = tableLayoutPanel1.GetControlFromPosition(x, y) as Label;
+            if (l.BackColor == Color.Black)
+                return true;
+            else
+                return false;
+        }
+        private bool PredictCellCondition(int x, int y)
+        {
+            bool outValue = false;
+            ruleDictionary.TryGetValue((GetCellCondition(x - 1, y - 1), GetCellCondition(x, y - 1), GetCellCondition(x + 1, y - 1)), out outValue);
+            return outValue;
+        }
+        private Label CreateLabel()
+        {
+            Label label = new Label();
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            label.Dock = DockStyle.Fill;
+            label.Margin = new System.Windows.Forms.Padding(0);
+            return label;
+        }
+        private void CreateRowNumberLabel(int i)
+        {
+            Label RowNumber = CreateLabel();
+            RowNumber.Text = Convert.ToString(i);
+            tableLayoutPanel1.Controls.Add(RowNumber, 0, i);
+        }
+        private void CellClick(object sender, EventArgs e)
+        {
+            Label cell = sender as Label;
+            ChangeCellColor(cell);
+        }
+        private void ChangeCellColor(Label cell)
+        {
+            if (cell.BackColor == Color.White)
+                cell.BackColor = Color.Black;
+            else
+                cell.BackColor = Color.White;
+        }
+        private void NewIteration()
+        {
+            for (int k = 1; k < 16; k++)
+            {
+                Label l = tableLayoutPanel1.GetControlFromPosition(k, i) as Label;
+                if (PredictCellCondition(k, i))
+                    l.BackColor = Color.Black;
+                else
+                    l.BackColor = Color.White;
+            }
+        }
+        private void FillTable()
+        {
+            for (int k = 0; k < 16; k++)
+            {
+                CreateRowNumberLabel(k);
+                for (int n = 1; n < 16; n++)
+                {
+                    Label TableLabel = CreateLabel();
+                    TableLabel.BackColor = Color.White;
+                    tableLayoutPanel1.Controls.Add(TableLabel, n, k);
+                }
+            }
+            for (int k = 1; k < 16; k++)
+                tableLayoutPanel1.GetControlFromPosition(k, 0).Click += new EventHandler(CellClick);
+        }
+        private void ClearTable()
+        {
+            for (int k = 1; k < 16; k++)
+                for (int n = 0; n < 16; n++)
+                {
+                    Label l = tableLayoutPanel1.GetControlFromPosition(k, n) as Label;
+                    l.BackColor = Color.White;
+                }
+        }
+        private void StartStopButton_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                SetRules();
+                timer1.Start();
+            }
+            else
+                timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NewIteration();
+            if (i < 15)
+                i++;
+            else
+                timer1.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillTable();
+            i = 1;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ClearTable();
+            i = 1;
         }
     }
 }
